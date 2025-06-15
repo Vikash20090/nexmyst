8af847cdc8bb2cc4da7aca3fe7f99529b4bdbfe1
Vikash Rajpurohit <vraj06100@gmail.com>
2025-06-15 13:19:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -621,3 +621,12 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log("✅ Service Worker Registered"))
+      .catch(err => console.log("❌ SW Error:", err));
+  });
+}
+
